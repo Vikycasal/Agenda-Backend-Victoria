59dbd9b74d35e135d38507a9dcbb3b35657a4cc3
Vikycasal <victoria.casalf@gmail.com>
2022-12-30 17:05:01 -0300

    cuentas

--- src/controllers/bills.controller.js
+++ src/controllers/bills.controller.js
@@ -33,3 +33,63 @@ export const getBill = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+//ruta para crear una cuenta nueva
+export const createBill = async (req, res) => {
+  const { title, date, amount } = req.body;
+
+  try {
+    const newBill = await Bills.create({
+      title,
+      date,
+      amount,
+    });
+
+    // console.log(newTask);
+    res.json(newBill);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Por algun motivo no se pudo crear la cuenta" });
+  }
+};
+
+//actualizar tarea
+export const updateBill = async (req, res) => {
+  try {
+    const { id } = req.params;
+    //aca le paso lo que yo quiero que actualice
+    const { title, date, amount } = req.body;
+    //tenemos que buscarlo en este caso por id
+    //aca estoy consultando y actualizando, ahora lo que tengo que hacer es guardarlo en la base de datos
+    const bill = await Bills.findByPk(id);
+    bill.title = title;
+    bill.date = date;
+    bill.amount = amount;
+    await bill.save();
+
+    res.json(bill);
+
+    // console.log(task);
+    // console.log(req.body);
+    res.json(bill);
+  } catch (error) {
+    return res.status(500).json({ message: "No se pudo actualizar la cuenta" });
+  }
+};
+
+//borrar cuenta
+export const deleteBill = async (req, res) => {
+  //cuando eliminamos un proyecto necesito un Id
+  try {
+    const { id } = req.params;
+    await Bills.destroy({
+      where: {
+        id,
+      },
+    });
+    res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: "No pudimos borrar lo solicitado" });
+  }
+};
--- src/routes/bills.route.js
+++ src/routes/bills.route.js
@@ -1,7 +1,19 @@
 import { Router } from "express";
+import {
+  createBill,
+  deleteBill,
+  getBill,
+  getBills,
+  updateBill,
+} from "../controllers/bills.controller";
 
 const router = Router();
 
 //Rutas
+router.get("/bills", getBills);
+router.get("/bills/:id", getBill);
+router.post("/bills", createBill);
+router.put("/bills/:id", updateBill);
+router.delete("/bills/:id", deleteBill);
 
 export default router;
